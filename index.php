4591572fa050ee6d7a5a7f24eeb89fbd4a772a01
Soisauz <55408184+Soisauz@users.noreply.github.com>
2020-04-27 19:34:44 +0700

    SB to producyion

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$server_key = "SB-Mid-server-7IzyRRPqY4zKOvFPzuYfpjzT";
+$server_key = "Mid-server-m43bifMZR6CJfFhoorxJ7Gv3";
 
-$is_production = false;
+$is_production = true;
 
 $api_url = $is_production ? 
   'https://app.midtrans.com/snap/v1/transactions' : 
